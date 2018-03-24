1ea854d9b189292870e354c666344fa00eb48975
MAUS502 <manngt@gmail.com>
2018-03-24 10:44:19 -0600

    Add room status to rooms

---
+++ database/migrations/2018_03_24_163834_add_room_status_to_rooms.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoomStatusToRooms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('rooms', function (Blueprint $table) {
+
+            $table->integer('room_status_id')->unsigned();
+
+            $table->foreign('room_status_id')
+
+                ->references('id')
+
+                ->on('room_statuses')
+
+                ->onDelete('restrict')
+
+                ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('rooms', function (Blueprint $table) {
+
+            $table->dropColumn('room_status_id');
+            
+        });
+
+    }
+}
