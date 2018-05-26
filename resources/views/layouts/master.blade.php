b005844fce0029e681405c223393c60b69938763
MAUS502 <manngt@gmail.com>
2018-05-26 09:40:14 -0600

    Add dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -128,6 +128,16 @@
 
 <script type="text/javascript" src="{{asset('materialize/js/scripts/dashboard-ecommerce.js')}}"></script>
 @yield('scripts')
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = 'a0f6bbb268f545d9e91aebace348436a6d2b43d3';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
 </body>
 
 </html>
--- resources/views/layouts/mastersite.blade.php
+++ resources/views/layouts/mastersite.blade.php
@@ -123,6 +123,17 @@
         $(".button-collapse").sideNav();
     });
 </script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+    _smartsupp.key = 'a0f6bbb268f545d9e91aebace348436a6d2b43d3';
+    window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+    })(document);
+</script>
 </body>
 
 </html>
