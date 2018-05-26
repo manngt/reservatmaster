75d34a5072ae6e83ce6a352d0714ae11d42252e2
MAUS502 <manngt@gmail.com>
2018-05-26 07:44:42 -0600

    Slider design

--- resources/views/layouts/topnavbarsite.blade.php
+++ resources/views/layouts/topnavbarsite.blade.php
@@ -1,10 +1,20 @@
 <div class="navbar-fixed">
     <nav class="navbar-color gradient-45deg-light-blue-cyan" >
         <div class="nav-wrapper">
-            <a href="/" class="brand-logo">Logo</a>
-            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="left">
+                <li>
+                    <h1 class="logo-wrapper">
+                        <a href="/" class="brand-logo darken-1">
+                            <img src="{{ asset('materialize/images/logo/iconreservat.png') }}" alt="ReservatMaster">
+                            <span class="logo-text hide-on-med-and-down">Reservat Master</span>
+                        </a>
+                    </h1>
+                </li>
+            </ul>
+
             <ul class="right hide-on-med-and-down">
                 <li><a href="/">Inicio</a></li>
+                <li><a href="/room">Login</a></li>
             </ul>
             <ul class="side-nav" id="mobile-demo">
                 <li><a href="{{route('room.index')}}">Inicio</a></li>
