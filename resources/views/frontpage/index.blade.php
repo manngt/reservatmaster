1675122a62000ff667c04fe78e45316806fb6a16
MAUS502 <manngt@gmail.com>
2018-05-30 15:23:15 -0600

    Room images col size

--- resources/views/frontpage/index.blade.php
+++ resources/views/frontpage/index.blade.php
@@ -36,7 +36,7 @@
 
                 @foreach($rooms as $room)
 
-                        <div class="col l6 s12 m6 xl6 sm6">
+                        <div class="col l6 s6 m6 xl6 sm6">
 
                             <div class="card-content ">
 
