40d62db44b5e7802f86fa63c2fb2cd2baa49a032
MAUS502 <manngt@gmail.com>
2018-05-30 15:15:38 -0600

    Room images col size

--- resources/views/frontpage/index.blade.php
+++ resources/views/frontpage/index.blade.php
@@ -36,11 +36,11 @@
 
                 @foreach($rooms as $room)
 
-                    <div class="card col l6 s6 m6 xl6">
+                    <div class="card col l4 s4 m4 xl4">
 
-                        <div class="card-content col l6 s6 m6 xl6">
+                        <div class="card-content ">
 
-                    <div class="card-img col l6 s6 m6 xl6">
+                    <div class="card-img">
                         <h1>{{$room->room_type->name}}</h1>
                         <p>{{$room->price}}</p>
                         <div class="carousel">
